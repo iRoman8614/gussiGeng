8e0f061cf9c3a64a3f38a1c07c00a5152637c147
Roman Baranovskii <rom8614@gmail.com>
2024-08-25 16:47:07 +0300

    fix window

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-import './App.css'
+import {useEffect} from "react";
 import {MainPage} from "./pages/main-page/MainPage.jsx";
 import { CoinProvider } from './context/CoinContext.jsx';
 
+import './App.css'
+
 function App() {
-  return (
+    useEffect(() => {
+        if (window.Telegram?.WebApp) {
+            window.Telegram.WebApp.setHeaderColor('#183256');
+            window.Telegram.WebApp.expand();
+        }
+    }, []);
+
+    return (
     <CoinProvider>
       <MainPage />
     </CoinProvider>
