cec053ecd661c1674b3a010dd47e753f3b4ac904
Roman Baranovskii <rom8614@gmail.com>
2024-08-25 13:55:47 +0300

    fix

--- src/components/buttons/big-btn/BigButton.jsx
+++ src/components/buttons/big-btn/BigButton.jsx
@@ -2,20 +2,9 @@ import styles from './BigButton.module.scss'
 
 // eslint-disable-next-line react/prop-types
 export const BigButton = ({image, title, alt}) => {
-    // const handleClick = () => {
-    //     if (window.Telegram?.WebApp?.HapticFeedback) {
-    //         window.Telegram.WebApp.HapticFeedback.impactOccurred('medium');
-    //     }
-    // };
-
     const handleClick = () => {
         if (window.Telegram?.WebApp?.HapticFeedback) {
-            console.log("HapticFeedback доступен");
-            window.Telegram.WebApp.HapticFeedback.impactOccurred('heavy');
-            alert('Вибрация вызвана');
-        } else {
-            console.log("HapticFeedback недоступен");
-            alert('API HapticFeedback недоступен');
+            window.Telegram.WebApp.HapticFeedback.impactOccurred('medium');
         }
     };
 
