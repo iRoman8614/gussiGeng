14e146fda0ffd615ad8822585c507a2b0ed45f5f
Roman Baranovskii <rom8614@gmail.com>
2024-09-01 22:50:28 +0300

    pvp

--- src/components/main-header/MainHeader.jsx
+++ src/components/main-header/MainHeader.jsx
@@ -2,7 +2,6 @@ import {IconButton} from "../buttons/icon-btn/IconButton.jsx";
 import {TotalBar} from "../bars/total-bar/TotalBar.jsx";
 
 import account from '../../../public/main-buttons/account.png'
-import gang from '../../../public/gangs-logos/purple-logo.png'
 import settings from '../../../public/main-buttons/settings.png'
 import boards from '../../../public/main-buttons/boards.png'
 import wallet from '../../../public/main-buttons/wallet.png'
@@ -13,14 +12,6 @@ import {useContext} from "react";
 
 export const MainHeader = () => {
     const { teamId, teamData } = useContext(CoinContext);
-
-    console.log('teamId', teamId)
-    console.log('teamData', teamData)
-    console.log('teamData[1]', teamData[1])
-    console.log('teamData[teamId]', teamData[teamId])
-    console.log('teamData[teamId].logo', teamData[teamId].logo)
-
-
 
     return(
         <div className={styles.root}>
