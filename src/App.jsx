6bd944e4559d2a36ece314fa39f7c7254226fc6b
Roman Baranovskii <rom8614@gmail.com>
2024-09-16 00:05:59 +0300

    request test

--- src/App.jsx
+++ src/App.jsx
@@ -62,7 +62,7 @@ function App() {
             ) : (
                 <HashRouter>
                     <Routes>
-                        <Route path='/' element={<MainPageWrapper setIsLoadingProfile={setIsLoadingProfile}  />} />
+                        <Route path='/' element={<MainPageWrapper setIsLoadingProfile={setIsLoadingProfile} userId={userId}  />} />
                         <Route path='/pvp' element={<PvpPage />} />
                     </Routes>
                 </HashRouter>
@@ -72,17 +72,17 @@ function App() {
 }
 
 // eslint-disable-next-line react/prop-types
-function MainPageWrapper() {
-    //надо прокидывать {userId}
-    // const [totalCoins, setTotalCoins] = useState(0);
-    // const [rate, setRate] = useState(3);
-    //
-    // console.log('totalCoins' , totalCoins)
-    // console.log('rate', rate)
-    //
-    // const { data: profile, error: profileError } = useGetProfileQuery(userId, {
-    //     skip: !userId,
-    // });
+function MainPageWrapper({userId}) {
+    // надо прокидывать {userId}
+    const [totalCoins, setTotalCoins] = useState(0);
+    const [rate, setRate] = useState(3);
+
+    console.log('totalCoins' , totalCoins)
+    console.log('rate', rate)
+
+    const { data: profile, error: profileError } = useGetProfileQuery(userId, {
+        skip: !userId,
+    });
 
     useEffect(() => {
         const savedTeamId = localStorage.getItem('teamId') || Math.floor(Math.random() * 4) + 1;
@@ -96,23 +96,23 @@ function MainPageWrapper() {
         localStorage.setItem('startFarmTime', savedStartFarmTime);
     }, []);
 
-    // useEffect(() => {
-    //     if (!profileError) {
-    //         console.log(`Sending request to https://supavpn.lol/farm/start?profileId=${profile.Id}`);
-    //         fetch(`https://supavpn.lol/farm/start?profileId=${profile.Id}`)
-    //             .then(response => response.json())
-    //             .then(farmData => {
-    //                 setRate(farmData.rate);
-    //                 setTotalCoins(profile.Balance);
-    //                 console.log("Farm start response:", farmData);
-    //             })
-    //             .catch((error) => {
-    //                 setRate(3);
-    //                 setTotalCoins(profile.Balance);
-    //                 console.error("Error during farm start request:", error);
-    //             });
-    //     }
-    // }, [profile, profileError]);
+    useEffect(() => {
+        if (!profileError) {
+            console.log(`Sending request to https://supavpn.lol/farm/start?profileId=${profile.Id}`);
+            fetch(`https://supavpn.lol/farm/start?profileId=${profile.Id}`)
+                .then(response => response.json())
+                .then(farmData => {
+                    setRate(farmData.rate);
+                    setTotalCoins(profile.Balance);
+                    console.log("Farm start response:", farmData);
+                })
+                .catch((error) => {
+                    setRate(3);
+                    setTotalCoins(profile.Balance);
+                    console.error("Error during farm start request:", error);
+                });
+        }
+    }, [profile, profileError]);
 
     return <MainPage />;
 }
