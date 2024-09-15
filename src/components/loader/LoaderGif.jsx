ae8b251edc80b894ddd33c93ff9430133d629a3c
Roman Baranovskii <rom8614@gmail.com>
2024-09-15 18:19:35 +0300

    pvp anims

--- src/components/loader/LoaderGif.jsx
+++ src/components/loader/LoaderGif.jsx
@@ -4,8 +4,7 @@ import loader from '/loading.gif'
 export const LoaderGif = () => {
     return(
         <div className={styles.root}>
-            <img className={styles.video}
-                 src={loader} alt="Loading..."/>
+            <img className={styles.video} src={loader} alt="Loading..."/>
         </div>
     )
 }
